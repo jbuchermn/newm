0ade9e390e4e9a4b78e1c44b1af8411b1ca56c1c
Jonas Bucher <j.bucher.mn@gmail.com>
2021-02-06 22:22:18 +0100

    Submodule pywm

--- main.sh
+++ main.sh
@@ -4,6 +4,8 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 cd $HOME
 
-$SCRIPTPATH/../make.sh || exit 1;
+echo "Making PyWM"
+$SCRIPTPATH/pywm/make.sh || exit 1;
+
 echo "Starting WM..."
 python -u $SCRIPTPATH/main.py ALT > $HOME/.cache/wm_log_$(date --iso-8601=seconds) 2>&1
