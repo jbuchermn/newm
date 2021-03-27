7bfa304e33929a5239cecd9a82d776d1417d696d
Jonas Bucher <j.bucher.mn@gmail.com>
2021-03-27 10:19:44 +0100

    Move panels to newm-panel-nwjs

--- panel/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-NW_PATH=/usr/lib/node_modules/nw/bin
-TMP_DIR=/tmp/.nw-$$
-
-mkdir -p $TMP_DIR
-
-$NW_PATH/nw $1/build --enable-features=UseOzonePlatform --ozone-platform=wayland --user-data-dir=$TMP_DIR
