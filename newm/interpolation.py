3919c70bc1b09334fb99f2931b166820f54a9663
Jonas Bucher <j.bucher.mn@gmail.com>
2021-10-21 23:44:08 +0200

    Accomodate Chromium CSDs using pywm mask mechanism

--- newm/interpolation.py
+++ newm/interpolation.py
@@ -27,6 +27,7 @@ class ViewDownstreamInterpolation(Interpolation[PyWMViewDownstreamState]):
     def __init__(self, state0: PyWMViewDownstreamState, state1: PyWMViewDownstreamState) -> None:
         self.z_index = (state0.z_index, state1.z_index)
         self.box = (state0.box, state1.box)
+        self.mask = (state0.mask, state1.mask)
         self.corner_radius = (state0.corner_radius, state1.corner_radius)
         self.accepts_input = state1.accepts_input
         self.size = (state0.size, state1.size)
@@ -41,9 +42,16 @@ class ViewDownstreamInterpolation(Interpolation[PyWMViewDownstreamState]):
             self.box[0][2] + (self.box[1][2] - self.box[0][2]) * at,
             self.box[0][3] + (self.box[1][3] - self.box[0][3]) * at,
         )
+        mask=(
+            self.mask[0][0] + (self.mask[1][0] - self.mask[0][0]) * at,
+            self.mask[0][1] + (self.mask[1][1] - self.mask[0][1]) * at,
+            self.mask[0][2] + (self.mask[1][2] - self.mask[0][2]) * at,
+            self.mask[0][3] + (self.mask[1][3] - self.mask[0][3]) * at,
+        )
         res = PyWMViewDownstreamState(
             z_index=self.z_index[1] if at > 0.5 else self.z_index[0],
             box=box,
+            mask=mask,
             corner_radius=(self.corner_radius[0] + at * (self.corner_radius[1] - self.corner_radius[0])),
             accepts_input=self.accepts_input
         )
