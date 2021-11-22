e6c81d6cc344f1db6321daa26c74f21653523cd5
Jonas Bucher <j.bucher.mn@gmail.com>
2021-11-22 18:51:14 +0100

    Minor fix

--- newm/util.py
+++ newm/util.py
@@ -1,10 +1,12 @@
 import time
-import sys
+import logging
+
+logger = logging.getLogger(__name__)
 
 def timed_function(func):  # type: ignore
     def wrapped(*args, **kwargs):  # type: ignore
         t = time.time()
         res = func(*args, **kwargs)
-        print("TIMER[%-30s]  : %fms" % (func.__name__, (time.time() - t) * 1000), file=sys.stderr)
+        logger.debug("TIMER[%-30s]  : %7.2fms" % (func.__name__, (time.time() - t) * 1000))
         return res
     return wrapped
