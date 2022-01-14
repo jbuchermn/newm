1703609ea746a61363b1058edc326280b7e05a50
Jonas Bucher <j.bucher.mn@gmail.com>
2022-01-14 19:50:59 +0100

    Fix massive performance issue in BacklightManager

--- newm/helper/backlight_manager.py
+++ newm/helper/backlight_manager.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 import logging
 import time
+import os
 
 from .execute import execute
 
@@ -28,39 +29,50 @@ class BacklightManager:
 
         self._predim = self._current
         self._next = self._current
-        self._anim_ts = 0., 0.
+        self._anim_ts = -1., -1., -1.
 
     def update(self) -> None:
-        if self._anim_ts[0] == 0.:
+        if not self._enabled or self._anim_ts[0] < 0.:
             return
+
         t = time.time()
+
+        dt = t - self._anim_ts[2]
+        if dt > 1. / 30.:
+            self._anim_ts = self._anim_ts[0], self._anim_ts[1], t
+        else:
+            return
+
         if t > self._anim_ts[1]:
             self._current = self._next
-            self._anim_ts = 0., 0.
+            self._anim_ts = -1., -1., -1.
         else:
             self._current = round(self._current + (self._next - self._current)/(self._anim_ts[1] - self._anim_ts[0])*(t - self._anim_ts[0]))
-        execute(self._commands[2](self._current) + " &")
+        os.system(self._commands[2](self._current) + " &")
 
     def callback(self, code: str) -> None:
         if code == "sleep":
             self._current = 1 # If set to zero, systemd will resume with 100%
             self._next = 1
-            execute(self._commands[2](self._current) + " &")
+            execute(self._commands[2](self._current))
             return
 
+        next = self._next
         if code == "wakeup":
-            self._next = self._predim
+            next = self._predim
         elif code in ["lock", "idle-lock"]:
-            self._next = round(self._predim * self._dim_factors[0])
+            next = round(self._predim * self._dim_factors[0])
         elif code == "idle":
-            self._next = round(self._predim * self._dim_factors[1])
+            next = round(self._predim * self._dim_factors[1])
         elif code == "idle-presuspend":
-            self._next = 0
+            next = 0
         elif code == "active":
-            self._next = self._predim
+            next = self._predim
 
-        if self._anim_ts[0] == 0.:
-            self._anim_ts = time.time(), time.time() + self._anim_time
+        if abs(next - self._next) > 0.5 and self._anim_ts[0] < 0:
+            t = time.time()
+            self._anim_ts = t, t + self._anim_time, 0.
+        self._next = next
 
     def adjust(self, factor: float) -> None:
         if self._predim < .3*self._max and factor > 1.:
@@ -69,4 +81,4 @@ class BacklightManager:
             self._predim = max(0, min(self._max, round(self._predim * factor)))
         self._next = self._predim
 
-        self._anim_ts = time.time(), time.time() + self._anim_time
+        self._anim_ts = time.time(), time.time() + self._anim_time, 0
