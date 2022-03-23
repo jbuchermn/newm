2d62e40e47333bd524132d4872adbb746c834f92
Crag <ogeidca49@gmail.com>
2022-03-22 21:24:03 -0600

    Fix minor issues

--- newm/cmd.py
+++ newm/cmd.py
@@ -4,19 +4,20 @@ import time
 
 from .dbus import send_dbus_command
 
+
 def cmd(command: str, *args: str) -> None:
     if command == "inhibit-idle":
         try:
-            send_dbus_command({'cmd': "inhibit-idle"})
+            send_dbus_command({"cmd": "inhibit-idle"})
             while True:
                 time.sleep(10)
         except:
             pass
         finally:
-            send_dbus_command({'cmd': "finish-inhibit-idle"})
+            send_dbus_command({"cmd": "finish-inhibit-idle"})
     elif command == "launcher":
-        send_dbus_command({'cmd': 'launcher', 'app': " ".join(args)})
+        send_dbus_command({"cmd": "launcher", "app": " ".join(args)})
     else:
-        result = send_dbus_command({'cmd': command, 'arg': " ".join(args)})
-        if result is not None and 'msg' in result:
-            print(result['msg'])
+        result = send_dbus_command({"cmd": command, "arg": " ".join(args)})
+        if result is not None and "msg" in result:
+            print(result["msg"])
